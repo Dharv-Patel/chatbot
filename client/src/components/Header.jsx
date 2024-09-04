2f25122a59d6d33f2bab875ca3154cfd99635028
Dharv-Patel <dharvpatel8524@gmail.com>
2024-09-05 00:24:44 +0530

    start to make Header and Home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook, faBookmark, faComment, faHouse, faRightToBracket, faUser } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function Header() {
+  const [hint, setHint] = useState(null);
+
+  const handelHint = (e) =>{
+    setHint(e.target.id)
+  }
   return (
-    <div className='bg-indigo-200 w-[4%] h-full sm:h-[100vh] fixed '>
-      <div className='flex flex-col'>
+    <div className='bg-slate-300 w-[4%] h-full sm:h-[100vh] fixed z-50 bg-opacity-50 shadow-xl'>
+      <div className='flex flex-col ml-4 items-start'>
+
+      <Link to='/' className='text-center mt-6 flex' onMouseOver={handelHint}>
+        <FontAwesomeIcon id='home' icon={faHouse} className='text-2xl text-blue-900' />
+        {/* {hint == 'home' ? <div className='bg-slate-300 w-20 ml-2 h-7 rounded' >Home</div> : ''} */}
+        
+      </Link>
+
+
+      <Link to='/chatbot' className='text-center mt-6 flex'>
+        <FontAwesomeIcon id='chatbot' icon={faComment} className='text-2xl text-blue-900 ml-1' />
+        {/* <div className='bg-slate-300 w-20 ml-2 h-7 rounded'>Chat</div> */}
+      </Link>
+
+      <Link to='/' className='text-center mt-6 flex'>
+        <FontAwesomeIcon id='bookmark' icon={faBookmark} className='text-2xl text-blue-900 ml-2' onMouseOver={handelHint}/>
+        {/* <div className='bg-slate-300 w-24 ml-2 h-7 rounded'>Bookmark</div> */}
+      </Link>
+
+      <Link to='/aunthicate' className='text-center mt-6 flex'>
+        <FontAwesomeIcon id='login' icon={faRightToBracket} className='text-2xl text-blue-900 border-b-2 border-slate-300 pb-3 w-8' onMouseOver={handelHint}/>
+        {/* <div className='bg-slate-300 w-20 h-7 ml-2 rounded'>login</div> */}
+      </Link>
+
+      <Link to='/profile' className='text-center mt-6 flex'>
+        <FontAwesomeIcon id='profile' icon={faUser} className='text-2xl text-blue-900 border-t-2 border-slate-300 pt-3 w-8 mt-[350px]' onMouseOver={handelHint}/>
+        {/* <div className='bg-slate-300 w-20 mt-[360px] h-7 ml-2 rounded'>Profile</div> */}
+      </Link>
+
+      <Link to='/' className='text-center mt-6 flex'>
+        <FontAwesomeIcon id='history' icon={faBook} className='text-2xl text-blue-900 ml-1' onMouseOver={handelHint}/>
+        {/* <div className='bg-slate-300 w-20 ml-2 h-7 rounded'>History</div> */}
+      </Link>
+
+      
         {/* <div className='font-bold sm:text-2xl text-xl cursor-pointer'>
           
           <Link to='/'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function Home() {
   return (
-    <div>
-        HomeHomeHomeHomeHomeHome
+    <div className='w-full h-full flex'>
+        {/* <div className='w-[4%] h-full'>s</div> */}
+
+        {/* website content */}
+        <div>
+          <div className='absolute top-[-30%] -z-20'>
+            <img className='w-full ' src="https://images.unsplash.com/photo-1471506480208-91b3a4cc78be?q=80&w=1774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          </div>
+
+
+          <div className='w-[650px] h-[600px] bg-slate-300 absolute top-[10%] left-[4%] bg-opacity-40 rounded-r-3xl'>
+            <div className='text-5xl font-bold font-sans leading-[46px] ml-10 mt-16 text-slate-900'>
+              <span>AI </span><span className='text-amber-600'>Assistant</span><br />
+              <span className='text-amber-600'>Chatbots </span><span>that</span><br />
+              <span>enhance the traveler experience</span>
+            </div>
+            <div className='font-mono font-bold mx-10 mt-10 text-slate-900 leading-5'>
+            Discover the world with ease! Our intelligent travel chatbot is your personal guide, ready to assist you in planning the perfect trip. Whether you're seeking hidden gems, need real-time recommendations, or want to navigate your journey smoothly.
+            </div>
+            <div className='font-mono font-bold mx-10 mt-4 text-slate-900 leading-5'>
+            our chatbot is here to make your travel experience seamless and unforgettable. Explore, ask, and embark on your next adventure with confidence!
+            </div>
+
+            <div className='flex justify-center w-full my-14'>
+              <Link to='/chatbot' className='w-64 h-12 bg-amber-600 text-center py-2 text-xl font-bold text-slate-900 rounded-lg bg-opacity-70 flex justify-evenly align-middle hover:bg-opacity-100'>
+                <div>Start Chat</div>
+                <FontAwesomeIcon id='home' icon={faArrowRight} className='text-2xl text-slate-900 mt-1' />
+              </Link>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
